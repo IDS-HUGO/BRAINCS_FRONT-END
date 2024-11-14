bf4b1f4c17037e32f072a9bf38a403a07eaa7806
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-11-14 08:59:40 -0600

    reorganized folders

--- src/app/docente/docente.module.ts
+++ src/app/docente/docente.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardGroupComponent } from './card-group/card-group.component';
-import { MenuModalComponent } from './menu-modal/menu-modal.component';
+import { CardGroupComponent } from './home-docente/card-group/card-group.component';
+import { MenuModalComponent } from './home-docente/menu-modal/menu-modal.component';
 import { ViewGroupComponent } from './view-group/view-group.component';
-import { CardActivityComponent } from './card-activity/card-activity.component';
+import { CardActivityComponent } from './view-group/card-activity/card-activity.component';
 import { AuthModule } from '../auth/auth.module';
 import { RoutingDocenteModule } from './routing-docente/routing-docente.module';
 import { HeaderGroupComponent } from '../layout/header-group/header-group.component';
 import { HomeDocenteComponent } from './home-docente/home-docente.component';
-import { ModalViewActivityComponent } from './modal-view-activity/modal-view-activity.component';
+import { ModalViewActivityComponent } from './view-group/modal-view-activity/modal-view-activity.component';
 
 @NgModule({
   declarations: [
