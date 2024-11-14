1c2a15db7fcd82474dfc4fc4a8851f888a4b7784
Alessandra Ulloa <233303@ids.upchiapas.edu.mx>
2024-11-13 18:39:13 -0600

    Add: Header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isClicked = false; 
 
+  toggleAddButton() {
+    this.isClicked = !this.isClicked; 
+  }
 }
