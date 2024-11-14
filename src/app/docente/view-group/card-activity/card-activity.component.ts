ffa4f838804161174057dc87efd528b3e8f807bf
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-11-14 09:21:22 -0600

    add activity modal

--- src/app/docente/view-group/card-activity/card-activity.component.ts
+++ src/app/docente/view-group/card-activity/card-activity.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-activity',
   templateUrl: './card-activity.component.html',
-  styleUrl: './card-activity.component.css'
+  styleUrls: ['./card-activity.component.css']
 })
 export class CardActivityComponent {
+  isModalVisible: boolean = false;  
+  selectedActivity: { name: string, description: string, dueDate?: Date } | null = null;
 
+  activity = {
+    name: 'Hola Mundo',
+    description: 'Crear un programa que imprima el mensaje de Hola Mundo en Java',
+    dueDate: new Date('2024-10-30')
+  };
+
+  openModal(activity: { name: string, description: string, dueDate?: Date }) {
+    this.selectedActivity = activity;
+    this.isModalVisible = true;
+  }
+
+  closeModal() {
+    this.isModalVisible = false;
+  }
+
+  handleSubmitActivity(file: File) {
+    console.log('Archivo recibido:', file);
+    this.closeModal();
+  }
 }
--- src/app/docente/view-group/modal-view-activity/modal-view-activity.component.ts
+++ src/app/docente/view-group/modal-view-activity/modal-view-activity.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal-view-activity',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './modal-view-activity.component.css'
 })
 export class ModalViewActivityComponent {
+  @Input() activityDetails: any;
+  @Output() close = new EventEmitter<void>();  
+  @Output() submitActivity = new EventEmitter<File>();
 
+  selectedFile: File | null = null;
+
+  onCloseModal() {
+    this.close.emit();
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
+  }
+
+  onSubmitActivity() {
+    if (this.selectedFile) {
+      this.submitActivity.emit(this.selectedFile);
+    } else {
+      alert('Por favor selecciona un archivo antes de subirlo.');
+    }
+  }
 }
