a1ccc89fe39765e24ce6c415a5be1e7680e2ec58
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-11-14 20:48:15 -0600

    add view group and temario

--- src/app/docente/docente.module.ts
+++ src/app/docente/docente.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { CardGroupComponent } from './home-docente/card-group/card-group.component';
 import { MenuModalComponent } from './home-docente/menu-modal/menu-modal.component';
 import { ViewGroupComponent } from './view-group/view-group.component';
-import { CardActivityComponent } from './view-group/card-activity/card-activity.component';
+import { CardActivityComponent } from './view-group/view-activity/card-activity/card-activity.component';
 import { AuthModule } from '../auth/auth.module';
 import { RoutingDocenteModule } from './routing-docente/routing-docente.module';
 import { HeaderGroupComponent } from '../layout/header-group/header-group.component';
 import { HomeDocenteComponent } from './home-docente/home-docente.component';
 import { ModalViewActivityComponent } from './view-group/modal-view-activity/modal-view-activity.component';
+import { ViewActivityComponent } from './view-group/view-activity/view-activity.component';
+import { ViewAlumnosComponent } from './view-group/view-alumnos/view-alumnos.component';
+import { ViewTemarioComponent } from './view-group/view-temario/view-temario.component';
+import { CardTemarioComponent } from './view-group/view-temario/card-temario/card-temario.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { ModalViewActivityComponent } from './view-group/modal-view-activity/mod
     ViewGroupComponent,
     CardActivityComponent,
     HomeDocenteComponent,
-    ModalViewActivityComponent
+    ModalViewActivityComponent,
+    ViewActivityComponent,
+    ViewAlumnosComponent,
+    ViewTemarioComponent,
+    CardTemarioComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/docente/view-group/view-temario/card-temario/card-temario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-temario',
+  templateUrl: './card-temario.component.html',
+  styleUrl: './card-temario.component.css'
+})
+export class CardTemarioComponent {
+
+}
