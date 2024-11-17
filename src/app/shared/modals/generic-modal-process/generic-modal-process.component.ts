63505dca4458fad7c4924de6a1859b3ae95cf4bb
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-11-16 19:35:47 -0600

    add modals genericas de procesos y mensajes

---
+++ src/app/shared/modals/generic-modal-process/generic-modal-process.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-modal-process',
+  templateUrl: './generic-modal-process.component.html',
+  styleUrl: './generic-modal-process.component.css'
+})
+export class GenericModalProcessComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GenericCardComponent } from './cards/generic-card/generic-card.component';
+import { GenericModalComponent } from './modals/generic-modal/generic-modal.component';
+import { GenericModalProcessComponent } from './modals/generic-modal-process/generic-modal-process.component';
 
 
 
 @NgModule({
   declarations: [
-    GenericCardComponent
+    GenericCardComponent,
+    GenericModalComponent,
+    GenericModalProcessComponent
   ],
   imports: [
     CommonModule
