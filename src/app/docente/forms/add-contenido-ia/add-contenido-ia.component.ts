2bb548b0e40f1e554d1b36a32a1c0861af51de94
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-11-25 01:40:15 -0600

    generate content whit ia

--- src/app/docente/forms/add-contenido-ia/add-contenido-ia.component.ts
+++ src/app/docente/forms/add-contenido-ia/add-contenido-ia.component.ts
@@ -1,11 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ModalService } from '../../../shared/modals/services/modal.service';
+import { PdfService } from '../../services/pdf.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-contenido-ia',
   templateUrl: './add-contenido-ia.component.html',
-  styleUrl: './add-contenido-ia.component.css'
+  styleUrls: ['./add-contenido-ia.component.css']
 })
+
 export class AddContenidoIAComponent {
   modalOpen: boolean = false;
 
@@ -14,21 +17,66 @@ export class AddContenidoIAComponent {
   selectedActivity: any = null;
   isModalOpen = false;
 
-  constructor(private modalService: ModalService) {
+  promptInput: string = '';
+  generatedContent: string = '';
+
+  constructor(
+    private modalService: ModalService,
+    private pdfService: PdfService
+  ) {
     this.modalService.modalOpen$.subscribe(isOpen => {
       this.modalOpen = isOpen;
     });
   }
 
   closeModal() {
-    this.modalService.openModal('contenido')
+    this.isModalOpen = false;
+  }
+
+  async generateContent() {
+    try {
+      this.generatedContent = '';
+      const content = await this.pdfService.generateContent(this.promptInput);
+  
+      let index = 0;
+      const interval = setInterval(() => {
+
+        this.generatedContent += content[index];
+        index++;
+  
+        if (index === content.length) {
+          clearInterval(interval);
+        }
+      }, 20);
+  
+    } catch (error) {
+      console.error('Error generando contenido:', error);
+      if (error instanceof HttpErrorResponse && error.status === 401) {
+        alert('No autorizado. Verifique su API Key.');
+      } else {
+        alert('Error al generar contenido.');
+      }
+    }
+  }
+  
+  generatePdf() {
+    try {
+      if (!this.generatedContent) {
+        alert('Primero debe generar contenido antes de crear el PDF.');
+        return;
+      }
+      const { pdfBlob, pdfUrl } = this.pdfService.generatePdf(this.generatedContent);
+      window.open(pdfUrl);
+      alert('PDF generado exitosamente.');
+      this.closeModal();
+    } catch (error) {
+      console.error('Error al generar PDF:', error);
+      alert('Error al generar el PDF.');
+    }
   }
 
   openContentModal(activity: any) {
     this.selectedActivity = activity;
     this.isModalOpen = true;
   }
-
-
-
 }
