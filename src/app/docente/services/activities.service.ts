cd89889ef79138bc956622a1fe41c2c9a0f4f47a
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-11-25 06:08:50 -0600

    service createActivity

--- src/app/docente/services/activities.service.ts
+++ src/app/docente/services/activities.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../enviroment/enviroment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivitiesService {
+  private readonly apiUrl = `${environment.apiUrl}actividad/`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createActivity(idGrupo: number, tema: string, subtema: string, contenido: File): Observable<any> {
+    const url = `${this.apiUrl}?id_grupo=${idGrupo}&tema=${tema}&subtema=${subtema}`;
+    const formData = new FormData();
+    formData.append('contenido', contenido);
+    return this.http.post(url, formData);
+  }
 }
