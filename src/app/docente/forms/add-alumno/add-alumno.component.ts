56738344d1c2aba4f050f9b3b8266cb8ed0f2610
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-11-30 13:09:54 -0600

    add create alumno

--- src/app/docente/forms/add-alumno/add-alumno.component.ts
+++ src/app/docente/forms/add-alumno/add-alumno.component.ts
@@ -1,22 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlumnosService } from '../../services/alumnos.service';
 import { ModalService } from '../../../shared/modals/services/modal.service';
+import { Alumno } from '../../models/alumno';
 
 @Component({
   selector: 'app-add-alumno',
   templateUrl: './add-alumno.component.html',
-  styleUrl: './add-alumno.component.css'
+  styleUrls: ['./add-alumno.component.css']
 })
-export class AddAlumnoComponent {
+export class AddAlumnoComponent implements OnInit {
 
-  constructor (private modalService : ModalService) {}
+  step = 1;
+  matricula : string = ''
+  nombres: string = '';
+  apellidoPaterno: string = '';
+  apellidoMaterno: string = '';
+  correoElectronico: string = '';
+  contrasena: string = '';
+  calificacion : number = 0;
+  
+  grado: number = 0;
+  grupo: string = '';
 
-  nombres : string = ""
-  apellidoPaterno : string = ""
-  apellidoMaterno : string = ""
+  constructor(
+    private route: ActivatedRoute,
+    private alumnosService: AlumnosService,
+    private modalService: ModalService
+  ) {}
 
-  closeModal(){
-    this.modalService.closeModal()
+  ngOnInit(): void {
+    this.grado = Number(this.route.snapshot.paramMap.get('grado'));
+    this.grupo = this.route.snapshot.paramMap.get('grupo') || '';
   }
 
+  closeModal(): void {
+    this.modalService.closeModal();
+  }
 
+  addAlumno(): void {
+    const nuevoAlumno: Alumno = {
+      matricula: this.matricula,
+      nombre: this.nombres,
+      apellido_p: this.apellidoPaterno,
+      apellido_m: this.apellidoMaterno,
+      correo_electronico: this.correoElectronico,
+      contrasena: this.contrasena,
+      grado: this.grado,
+      grupo: this.grupo,
+      calif_total : this.calificacion
+    };
+
+    this.alumnosService.createAlumno(nuevoAlumno).subscribe({
+      next: () => {
+        alert('Alumno agregado exitosamente.');
+        this.closeModal();
+      },
+      error: (err) => {
+        console.error('Error al agregar alumno:', err);
+        alert('Ocurrió un error al agregar el alumno.');
+      }
+    });
+  }
+
+  nextStep() {
+    if (this.step < 3) {
+      this.step++;
+    }
+  }
+
+  prevStep() {
+    if (this.step > 1) {
+      this.step--;
+    }
+  }
 }
--- src/app/docente/services/alumnos.service.ts
+++ src/app/docente/services/alumnos.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../enviroment/enviroment';
+import { Alumno } from '../models/alumno';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlumnosService {
-  private apiUrl = `${environment.apiUrl}alumnos/alumnos/grado/`;
+  private getAlumnoByGradoGrupo = `${environment.apiUrl}alumnos/alumnos/grado/`;
+  private addAlumno = `${environment.apiUrl}alumnos/alumnos/`;
 
   constructor(private http: HttpClient) { }
 
   getAlumnos(grado: number, grupo: string): Observable<any> {
-    const url = `${this.apiUrl}${grado}/grupo/${grupo}`;
+    const url = `${this.getAlumnoByGradoGrupo}${grado}/grupo/${grupo}`;
     return this.http.get<any[]>(url);
+  }
+
+  createAlumno(alumno: Alumno): Observable<any> {
+    return this.http.post<any>(this.addAlumno, alumno);
   }
 }
