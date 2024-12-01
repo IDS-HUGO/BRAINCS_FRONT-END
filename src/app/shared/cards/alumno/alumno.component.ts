d1d2ad5826523ac4d3b3099bae5bdddfc63867a2
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-12-01 14:32:52 -0600

    add inspect-alumno

--- src/app/shared/cards/alumno/alumno.component.ts
+++ src/app/shared/cards/alumno/alumno.component.ts
@@ -1,16 +1,29 @@
-import { Component, Input, Output, EventEmitter  } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ModalService } from '../../modals/services/modal.service';
+import { AlumnoSelectedService } from '../services/alumno-selected.service';
 
 @Component({
   selector: 'app-alumno',
   templateUrl: './alumno.component.html',
-  styleUrl: './alumno.component.css'
+  styleUrls: ['./alumno.component.css']
 })
 export class AlumnoComponent {
   @Input() cardData: any;
   @Input() isGroupView: boolean = true;
   @Output() openModal = new EventEmitter<any>();
   showModal: boolean = false;
-  alumno : string = ''
+
+  constructor (
+    private modalService : ModalService,
+    private alumnoSelectedService: AlumnoSelectedService
+  ) {}
+
+  onCardClick(event: MouseEvent) {
+    event.stopPropagation();
+    this.alumnoSelectedService.setSelectedAlumno(this.cardData);
+    this.openModal.emit(this.cardData);
+    this.modalService.openModal('inspect-alumno');
+  }  
 
   toggleModal() {
     this.showModal = !this.showModal;
