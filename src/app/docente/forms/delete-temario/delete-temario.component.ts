7fd4c128dfe783dfbaa1ceac9df753327d55415d
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-12-01 11:22:42 -0600

    add inspect temario

---
+++ src/app/docente/forms/delete-temario/delete-temario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-temario',
+  templateUrl: './delete-temario.component.html',
+  styleUrl: './delete-temario.component.css'
+})
+export class DeleteTemarioComponent {
+
+}
--- src/app/shared/cards/temario/temario.component.ts
+++ src/app/shared/cards/temario/temario.component.ts
@@ -1,28 +1,42 @@
-import { Component, Input, Output, EventEmitter  } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ModalService } from '../../modals/services/modal.service';
+import { TemarioSelectedService } from '../services/temario-selected.service';
 
 @Component({
   selector: 'app-temario',
   templateUrl: './temario.component.html',
-  styleUrl: './temario.component.css'
+  styleUrls: ['./temario.component.css']
 })
 export class TemarioComponent {
+  @Input() userType: string = 'alumno';
   @Input() cardData: any;
   @Input() isGroupView: boolean = true;
-  @Input() userType: string = 'alumno';
   @Output() openModal = new EventEmitter<any>();
+  temarioId : number = 0;
   showModal: boolean = false;
 
-  toggleModal() {
+  ngOnInit() {
+    console.log('Datos recibidos en Temario:', this.cardData);
+  }  
+
+  constructor(
+    private modalService: ModalService,
+    private temarioSelected : TemarioSelectedService
+  ) {}
+
+  toggleModal(event : MouseEvent) {
+    event.stopPropagation();
     if (this.userType !== 'alumno') {
       this.showModal = !this.showModal;
     }
   }
 
-  onCardClick() {
-    this.openModal.emit(this.cardData);
-  }
-
-
+  onCardClick(event: MouseEvent) {
+    event.stopPropagation();
+    console.log('Emitir evento con datos:', this.cardData);
+    this.temarioSelected.setSelectedTemario(this.cardData);
+    this.modalService.openModal('inspect-temario');
+  }  
 
   handleOption(option: string) {
     switch (option) {
